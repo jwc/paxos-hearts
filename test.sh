f097fabae7aaf6007ad02172b6b6439332901fee
jwc <24704574+jwc@users.noreply.github.com>
2026-03-04 23:20:05 -0500

    Improved test script.

--- test.sh
+++ test.sh
@@ -1,52 +1,92 @@
 #!/bin/env bash
 
-cd ./test
+RC='\033[0;0m'
+C1='\033[0;31m'
+C2='\033[0;32m'
+C3='\033[0;33m'
+C4='\033[0;34m'
+C5='\033[0;35m'
+C6='\033[0;36m'
 
-tests=$(ls test*.cc | rev | cut -c 4- | rev)
-nTests=$(ls test*.cc | wc -l)
-#nTests=$(echo $tests | wc -l)
+nTestsGlobal=0
+nPassedGlobal=0
+verbose=0
 
-passed=0
-#failed=0
+testSets="paxos"
 
-C1='\033[0;34m'
-C2='\033[0;0m'
-C3='\033[0;32m'
-C4='\033[0;31m'
-C5='\033[0;36m'
-
-#for i in $(seq 1 $nTests)
-#for i in $(ls test*.cc | rev | cut -c 4- | rev | cut -c 5-)
-#for i in $(ls Test*.cc | rev | cut -c 4- | rev)
-#for i in $(echo $tests | rev | cut -c 4- | rev)
-for i in $tests
-do
-  echo -e ${C1}$i:${C2} | sed 's/_/ /g'
-  cat ./${i}.cc | grep " \* "
-
-  #echo -e ${C1}Test $i:${C2} 
-  #cat ./test${i}.cc | grep " \* "
-
-  if [ -f ${i}.out ]
+run_test() {
+  if [ -f $1.out ]
   then
-    ./${i}.out &> ${i}.log
+    ./$1.out &> $1.log
 
     if [ $? = 0 ]
     then
-      echo -e "\t${C3}Passed.${C2}"
-      ((passed++))
+      echo -e "    ${C2}Passed.${RC}"
+      return 0
     else
-      echo -e "\t${C4}Failed.${C2}"
-      #((failed++))
+      echo -e "    ${C1}Failed.${RC}"
+      return 1
     fi
   else 
-    echo -e "\t${C4}Missing.${C2}"
+    echo -e "    ${C1}Missing.${RC}"
+    return 2
   fi
-  #./test${i}.out &> test${i}.log
-  #./test${i}.out 
+}
 
+run_test_set() {
+  tests=$(ls $1*.cc | rev | cut -c 4- | rev)
+  nTests=$(ls $1*.cc | wc -l)
+  ((nTestsGlobal+=nTests))
+  nPassed=0
+  
+  echo -e "\n${C5}Running $1 tests${RC}:"
+
+  for test in $tests
+  do 
+    echo -e "${C3}${test}:${RC}" | sed 's/_/ /g'
+    if [ "$verbose" = "1" ]
+    then
+      cat ./${test}.cc | grep " \* "
+    fi
+
+    run_test $test
+
+    if [ $? = 0 ]
+    then
+      ((nPassed++))
+      ((nPassedGlobal++))
+    fi
+  done
+
+  echo -e "${C4}${nPassed}/${nTests} testcases passed.${RC}"
+  return $nTests
+}
+
+cd ./test
+
+if [ $# = 1 ] && [ "$1" = "-v" ] 
+then
+  verbose=1
+elif [ $# != 0 ] 
+then 
+  testSets=$@
+fi
+
+for arg in $testSets
+do
+  if [ $arg = "-v" ]
+  then
+    verbose=1
+  else 
+    run_test_set $arg
+  fi
 done
 
-wait
-echo -e ${C5}${passed}/${nTests} testcases passed.${C2}
+if [ $nTests != $nTestsGlobal ] 
+then
+  echo -e "\n${C5}Total${RC}:"
+  echo -e "${C6}${nPassedGlobal}/${nTestsGlobal} testcases passed.${RC}"
+fi
+
+exit $(( $nTestsGlobal - $nPassedGlobal ))
 
