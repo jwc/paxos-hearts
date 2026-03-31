13d17e0843d5070b0644b859614d1d17eed0e616
jwc <24704574+jwc@users.noreply.github.com>
2026-03-31 17:10:40 -0400

    Removed unnecessary code.

--- src/interactive.cc
+++ src/interactive.cc
@@ -2,25 +2,6 @@
 
 char currentPlayer = 0;
 
-//void foo(Cards * c, Buffer<Value> * buf) {
-void foo(Hearts * h, Buffer<Value> * buf) {
-  //Cards * c = new Cards();
-  mvprintw(8, 3, "foo() created");
-  Value v;
-  int bar = 0;
-  while (1) {
-    mvprintw(8, 3, "foo() waiting");
-    v = buf->consume();
-    
-    mvprintw(2, 0, "%d foo() printing %c/%d\n", bar++, v.type, v.type);
-    //c->printCards(Printable::sizeY-3, Printable::sizeX/2, 0);
-    h->print(currentPlayer);
-    mvprintw(Printable::sizeY-1, Printable::sizeX/2, "X");
-  }
-  
-  mvprintw(8, 3, "foo() done\n");
-}
-
 int main(int argc, char** argv) {
   if (argc < 2) {
     exit(1);
@@ -28,25 +9,15 @@ int main(int argc, char** argv) {
 
   Networking *net = new Localhost(argv[1]);
   Paxos *pax = new Paxos(*net);
-  //Cards * c = new Cards();
   Hearts * h = new Hearts(*pax);
   for (int i = 0; i < 4; i++)
     h->hands[i].add(i);
-  //mvprintw(35, 0, "# threads %d", std::thread::hardware_concurrency());
-  Buffer<Value> * buf = new SVBuffer<Value>(1);
-
-  //std::jthread output = std::jthread(&foo, &pax->getBuffer());
-  //std::jthread output = std::jthread(&foo, c, buf);
-  std::jthread output = std::jthread(&foo, h, buf);
 
   mvprintw(0, 0, "main() ready\n");
   char input = 'a';
-  int count = 0;
   Value val;
   char hasExited = 0;
   while ( ! hasExited) {
-    //std::this_thread::yield();
-    //producer.lock();
     input = getch();
     fprintf(stderr, "input: %c\n", input);
 
@@ -117,10 +88,7 @@ int main(int argc, char** argv) {
         break;
     };
 
-    mvprintw(0, 0, "%d main() req(%c/%d)\n", count++, input, input);
-    buf->produce(val);
-    //std::this_thread::yield();
-    //consumer.unlock();
+    h->print(currentPlayer);
   } 
 
   mvprintw(0, 0, "main() done\n");
