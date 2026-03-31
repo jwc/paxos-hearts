2de9d777eb888e67e6f77ee8bacc3536d78c030f
jwc <24704574+jwc@users.noreply.github.com>
2026-03-31 16:50:49 -0400

    Replaced chain if statements with switch statement.

--- src/interactive.cc
+++ src/interactive.cc
@@ -5,11 +5,11 @@ char currentPlayer = 0;
 //void foo(Cards * c, Buffer<Value> * buf) {
 void foo(Hearts * h, Buffer<Value> * buf) {
   //Cards * c = new Cards();
-  mvprintw(2, 3, "foo() created");
+  mvprintw(8, 3, "foo() created");
   Value v;
   int bar = 0;
   while (1) {
-    mvprintw(2, 3, "foo() waiting");
+    mvprintw(8, 3, "foo() waiting");
     v = buf->consume();
     
     mvprintw(2, 0, "%d foo() printing %c/%d\n", bar++, v.type, v.type);
@@ -18,7 +18,7 @@ void foo(Hearts * h, Buffer<Value> * buf) {
     mvprintw(Printable::sizeY-1, Printable::sizeX/2, "X");
   }
   
-  mvprintw(0, 0, "foo() done\n");
+  mvprintw(8, 3, "foo() done\n");
 }
 
 int main(int argc, char** argv) {
@@ -40,66 +40,88 @@ int main(int argc, char** argv) {
   std::jthread output = std::jthread(&foo, h, buf);
 
   mvprintw(0, 0, "main() ready\n");
-  Value v;
-  v.type = 'a';
+  char input = 'a';
   int count = 0;
-  while (v.type != 'q') {
+  Value val;
+  char hasExited = 0;
+  while ( ! hasExited) {
     //std::this_thread::yield();
     //producer.lock();
-    v.type = getch();
-    fprintf(stderr, "input: %c\n", v.type);
+    input = getch();
+    fprintf(stderr, "input: %c\n", input);
 
-    if (v.type == 'r') {
-        Value val = { .type = REQ_START_T, 
-          .player = currentPlayer, 
-          .data = (int16_t) rand() };
+    switch (input) {
+      case 'q':
+        hasExited = 1;
+        break; 
+
+      case 't':
+        h->toggleUnicode();
+        h->toggleColor();
+        break;
+
+      case 'r':
+        val = { .type = REQ_START_T, 
+                .player = currentPlayer, 
+                .data = (int16_t) rand() };
         h->play(val);
-    } else if (v.type == 't') {
-      h->toggleUnicode();
-      h->toggleColor();
-    } else if (v.type == 'a') {
-      h->hands[rand() % 4].add(rand() % 52);
-    } else if (v.type == 'R') {
-      fprintf(stderr, "calling play()\n");
-      for (char i = 0; i < 4; i++) {
-        Value val = { .type = REQ_START_T, .player = i, .data = (int16_t) rand() };
-        h->play(val);
-      }
-    } else if (v.type == '0') {
-      currentPlayer = 0;
-    } else if (v.type == '1') {
-      currentPlayer = 1;
-    } else if (v.type == '2') {
-      currentPlayer = 2;
-    } else if (v.type == '3') {
-      currentPlayer = 3;
-    } else if (v.type == '4') {
-      currentPlayer = 0; 
-    } else if (v.type == 'l') {
-      Value val = { .type=SELECT_T, 
-                    .player=currentPlayer, 
-                    .data=h->hands[currentPlayer].getSelected() + 1 };
-      pax->sendStatelessValue(val);
-    } else if (v.type == 'h') {
-      Value val = { .type=SELECT_T, 
-                    .player=currentPlayer, 
-                    .data=(int16_t) h->hands[currentPlayer].getSelected() - 1 };
-      pax->sendStatelessValue(val);
-    } else if (v.type == ' ') {
-      //if (count != 0) continue;
-      Value val = { .type=PLAY_T,
-                    .player=currentPlayer, 
-                    .data=(int16_t) h->hands[currentPlayer].getSelected() };
-      //pax->sendStatelessValue(val);
-      pax->makeRequest(val);
-    }
-    mvprintw(0, 0, "%d main() req(%c/%d)\n", count++, v.type, v.type);
-    //pax->makeRequest(v);
-    buf->produce(v);
+        break;
+
+      case 'R':
+        fprintf(stderr, "calling play()\n");
+        for (char i = 0; i < 4; i++) {
+          val = { .type = REQ_START_T, .player = i, .data = (int16_t) rand() };
+          h->play(val);
+        }
+        break;
+
+      case 'l':
+        val = { .type=SELECT_T, 
+                .player=currentPlayer, 
+                .data=h->hands[currentPlayer].getSelected() + 1 };
+        pax->sendStatelessValue(val);
+        break;
+
+      case 'h':
+        val = { .type=SELECT_T, 
+                .player=currentPlayer, 
+                .data=(int16_t) h->hands[currentPlayer].getSelected() - 1 };
+        pax->sendStatelessValue(val);
+        break;
+
+      case ' ':
+        val = { .type=PLAY_T,
+                .player=currentPlayer, 
+                .data=(int16_t) h->hands[currentPlayer].getSelected() };
+        pax->makeRequest(val);
+        break;
+
+      case '0':
+        currentPlayer = 0;
+        break;
+
+      case '1':
+        currentPlayer = 1;
+        break;
+
+      case '2':
+        currentPlayer = 2;
+        break;
+
+      case '3':
+        currentPlayer = 3;
+        break;
+
+      case '4':
+        currentPlayer = 0;
+        break;
+    };
+
+    mvprintw(0, 0, "%d main() req(%c/%d)\n", count++, input, input);
+    buf->produce(val);
     //std::this_thread::yield();
-    //q.push(v);
     //consumer.unlock();
-  }
+  } 
 
   mvprintw(0, 0, "main() done\n");
   exit(0);
